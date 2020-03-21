b9def0fc75f5c12c392491104310709b818a8e3b
Kenneth Gilmore <gilmorekenneth@gmail.com>
2020-03-21 00:14:25 -0400

    basic functionality working

--- app.rb
+++ app.rb
@@ -19,6 +19,7 @@ end
 
 post '/game' do
   session[:guess_list] << params.values
-  @current_guess = Guess.new
+  @current_guess = Guess.new(session[:guess_list].last)
+  session[:key_list] << @current_guess.hint_key
   erb :game
 end
--- lib/guess.rb
+++ lib/guess.rb
@@ -1,47 +1,39 @@
 require_relative "game"
 
 class Guess
-  attr_reader :current_guess, :temp_key, :hint_key
+  attr_reader :hint_key, :temp_key, :current_guess
 
-  def initialize
-    @current_guess = session[:guess_list].last
-    @temp_key = @secret_key
-    # @hint_key = get_hint_key
+  def initialize(guess)
+    @current_guess = guess.slice(0..3)
+    @temp_key = GAME.secret_key.slice(0..3)
+    @hint_key = get_hint_key
+  end
+
+  def get_hint_key
+    key_array = []
+    guess_copy = @current_guess.dup
+    key_copy = @temp_key.dup
+
+    guess_copy.each_with_index do |val, i|
+      if guess_copy[i] == key_copy[i]
+        key_copy[i] = nil
+        guess_copy[i] = nil 
+        key_array << "red"
+      end
+    end 
+
+    guess_copy.each_with_index do |guess, i|
+      key_copy.each_with_index do |key, t|
+        if guess_copy[i] == key_copy[t] && !guess_copy[i] == key_copy[t]
+          guess_copy[i] = nil
+          temp_key[t] = nil
+          key_array << "white"
+        end
+      end
+    end
+    
+    key_array << "black" until key_array.length == 4
+    key_array
   end
 end
-#   def get_hint_key
-#     correct = show_correct
-#     hints = show_hints
-#     hint_array = make_hint_array(correct, hints)
-#     session[:key_list] << hint_array
-#   end 
 
-#   def show_correct 
-#     correct = 0
-#     @current_guess.each_with_index do |val, i|
-#       if @current_guess[i] == @temp_key[i]
-#         @temp_key[i] = nil 
-#         correct += 1
-#       end
-#     end 
-#     correct
-#   end
-
-#   def show_hints
-#     hints = 0
-#     @current_guess.each do |guess|
-#       hints += 1 if @temp_key.include?(guess)
-#     end
-#     hints
-#   end
-  
-#   def make_hint_array (correct, hints)
-#     hint_array = []
-#     correct.times {hint_array.unshift("red")}
-#     hints.times {hint_array.unshift("white")}
-#     while hint_array.length < 4
-#       hint_array.push("black")
-#     end
-#     hint_array
-#   end 
-# end
