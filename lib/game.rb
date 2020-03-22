970fd3a940c4a0ad1b4c2d044bed0d99d4e8808a
Kenneth Gilmore <gilmorekenneth@gmail.com>
2020-03-22 19:03:14 -0400

    Working, need front end though

--- lib/game.rb
+++ lib/game.rb
@@ -1,47 +1,59 @@
 require_relative "guess"
 
 class Game
-  attr_reader :secret_key, :round_count, :end_message
+  attr_reader :secret_key, :round_count, :message, :colors, :color_key
   
   def initialize
-    @secret_key = generate_key()
-    @round_count = 0
-    @end_message = ""
+    @colors = {
+      :red =>  " #f44336 " ,
+      :green => " #27ae60 ",
+      :blue => " #3498db ",
+      :yellow => " #f1c40f ",
+      :orange => " #e67e22 ",
+      :purple => " #8e44ad "
+    }
+    @secret_key = generate_key
+    @color_key = generate_color_key
+    @round_count = 1
+    @message = "Go ahead and make a guess!"
   end
 
-  colors = {
-    :red => " #c0392b ",
-    :green => " #27ae60 ",
-    :blue => " #3498db ",
-    :yellow =>
-    :orange =>
-    :purple =>
-  }
+  def generate_color_key 
+    array = []
+    @secret_key.each do |value|
+      @colors.each_pair do |k, v|
+        array << k if value == v 
+      end
+    end
+    array.join(", ")
+  end
 
   def generate_key 
-    4.times.map do 
-      [" #c0392b "," #27ae60 "," #3498db "," #f1c40f "," #e67e22 "," #8e44ad "].sample
-    end
+    @colors.values.sample(4)
   end
 
   def check_win(guess)
-    if guess == @secret_key && @round_count < 12
+    @round_count += 1
+    if guess == @secret_key 
       winner
-    elsif @round_count == 12
+    elsif @round_count > 12
       loser
     else
-      @round_count += 1
+      @message = message_maker
     end
   end
 
   def winner 
-    win = @secret_key.join(", ")
-    @end_message = "Congrats! You correctly guessed: #{win}!" 
+    @message = "Congrats! You correctly guessed: #{@color_key}!" 
   end
 
   def loser
-    loss = @secret_key.join(", ")
-    @end_message = "Aww shucks! the secret key was #{loss}"
+    @message = "Aww shucks! the secret key was #{@color_key}"
+  end
+
+  def message_maker
+    collection = ["I'm pretty sure there is a purple somewhere!", "You call that a guess?! Bwahahahaha", "I've seen better guesses from a mere baby!", "Are you even trying to win?"]
+    message = collection.sample(1).join("")
   end
 end
   
